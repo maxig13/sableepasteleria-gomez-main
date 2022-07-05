291bc01a17ad69a7f6277be25fb17df0abc818b6
maxig13 <lemaxg98@gmail.com>
2022-07-05 19:34:41 -0300

    git ignored corregido

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+/* Login de usuario
+
 let usuario = '';
 
 while (usuario != 'Maxi') {
@@ -18,4 +20,44 @@ while (contrasena != '1234'){
     }else{
         alert('Contraseña incorrecta, tu no eres Maxi');
     }   
-}
+} */
+
+// Carrito de compras
+
+//Variables globales
+let item = ['Torta brownie', 'Alfajor de maicena', 'Budin de limon'];
+let accion;
+let precio;
+
+//Menu
+do {
+    accion = Number(prompt('Seleccione una accion: \n\n1 - Seleccion de productos\n2 - Proceder al pago\n3 - Vaciar carrito\n4 - Salir'));
+    switch (accion) {
+        case 1:
+            do {
+                item = Number(prompt(`Seleccione un producto: \n\n1 - Torta brownie\n2 - Alfajor de maicena\n3 - Budin de limon\n4 - Salir`));
+                switch (item) {
+                    case 1:
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+            }while(item !==4);
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            break;
+    
+        default:
+            alert(`Opcion incorrecta`)
+            break;
+    }
+
+}while(accion !== 4);
